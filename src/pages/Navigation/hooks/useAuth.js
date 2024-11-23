752efafa2e9b6dc202cd314d8b1f1e63b19a0fee
Ankit Kumar <ankitkumar1578114@gmail.com>
2024-11-23 14:11:47 +0530

    Eslint desabled

--- src/pages/Navigation/hooks/useAuth.js
+++ src/pages/Navigation/hooks/useAuth.js
@@ -1,32 +1,40 @@
-import axios from 'axios'
+import axios from "axios";
 
 const useAuth = ({ setUser, setUserLoaded }) => {
   const responseMessage = async (response) => {
     try {
       const config = {
         headers: {
-          Authorization: `Bearer ${response.credential}`
-        }
-      }
-      const res = await axios.post(process.env.REACT_APP_BACKEND + 'login/', {}, config)
-      localStorage.setItem('token', response.credential)
-      localStorage.setItem('profileData', JSON.stringify(res?.data?.profileData))
-      setUser(res?.data?.profileData)
-      setUserLoaded(true)
+          Authorization: `Bearer ${response.credential}`,
+        },
+      };
+      const res = await axios.post(
+        process.env.REACT_APP_BACKEND + "login/",
+        {},
+        config
+      );
+      localStorage.setItem("token", response.credential);
+      localStorage.setItem(
+        "profileData",
+        JSON.stringify(res?.data?.profileData)
+      );
+      setUser(res?.data?.profileData);
+      setUserLoaded(true);
     } catch (err) {
-      setUserLoaded(true)
-      console.log(err, 'err')
-      localStorage.removeItem('token')
-      localStorage.removeItem('profileData')
+      if (err.response.status === 403) {
+        setUserLoaded(true);
+        localStorage.removeItem("token");
+        localStorage.removeItem("profileData");
+      }
     }
-  }
+  };
   const errorMessage = (error) => {
-    console.log(error)
-  }
+    console.log(error);
+  };
 
   return {
     responseMessage,
-    errorMessage
-  }
-}
-export default useAuth
+    errorMessage,
+  };
+};
+export default useAuth;
