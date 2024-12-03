51382ab32b56acf89a450d5b920f7fe8a3c1701b
ysahil780 <ysahil780@gmail.com>
2024-12-04 00:16:25 +0530

    players Update

--- src/pages/Player/List/index.js
+++ src/pages/Player/List/index.js
@@ -11,6 +11,8 @@ import { columns } from "../utlis/player-table";
 import layoutStyle from "../../Components/Layout/styles.module.css";
 import useListPlayers from "../hooks/useListPlayers";
 import AddPlayerLayout from "../AddPlayerLayout";
+import UploadPlayersList from "../UploadList";
+import Tabs from "../../Components/Tabs";
 
 const List = ({
   players: playersFromDashboard,
@@ -18,6 +20,7 @@ const List = ({
   primaryCall = true,
 }) => {
   const [players, setPlayers] = useState([]);
+  const [active, setActive] = useState(0);
   const [loading, setLoading] = useState(false);
   const [searchText, setSearchText] = useState("");
   const {
@@ -37,16 +40,28 @@ const List = ({
   }, [JSON.stringify(playersFromMain)]);
 
   const [show, setShow] = useState(false);
+  
+
   return (
     <>
-      <Modal show={show} setShow={setShow} size="md">
+    <Modal show={show} setShow={setShow} size="md">
+       <UploadPlayersList setShow ={setShow} listPlayers = {listPlayers}/>
+       </Modal>
+ 
+      {/* <Modal show={show} setShow={setShow} size="md">
         <AddPlayerLayout setShow={setShow} onCreatePlayers={listPlayers} />
-      </Modal>
-
+      </Modal> */}
       <div className={globalStyle.container}>
-        <div className={globalStyle.flex_right}>
+        <Tabs
+        tabs={["Your Players", "Search and Add players"]}
+        active={active}
+        onChange={(index) => setActive(index)}
+      />
+        <div>
+         {active == 1 && (
+         <div className={globalStyle.flex_right}>
           <div className={globalStyle.heading}>Players</div>
-          {/* <input
+          <input
             type="text"
             placeholder="Search player..."
             className={layoutStyle.input}
@@ -54,13 +69,16 @@ const List = ({
             onChange={(e) => {
               setSearchText(e.target.value);
             }}
-          /> */}
-          <Button value="+" onClick={() => setShow(true)} />
-        </div>
+          />
+          <Button value= "+"/>
+          <Button value="Upload Players List" onClick={() => setShow(true)} />
+          {/* <Button value="+" onClick={() => setShow(true)} /> */}
+        </div>)}
         <div className={globalStyle.table_content}>
           <Table columns={columns} data={players} loading={loading} />
         </div>
       </div>
+        </div>
     </>
   );
 };
---
+++ src/pages/Player/hooks/useBulkUploadPlayers.js
@@ -0,0 +1,51 @@
+import useRequest from "../../../common/hooks/useRequest";
+import { useDispatch } from "react-redux";
+import { setToast } from "../../../common/store/toastSlice";
+
+const useBulkUploadPlayers = ({ listPlayers = () => {}, setShow = () => {} }) => {
+  const dispatch = useDispatch();
+
+  const { data, loading, trigger } = useRequest({
+    url: 'bulk-upload-players',
+    method: 'post',
+    isConfig: true
+  })
+
+  const bulkUploadPlayers = async (data) => {
+    try{
+      const payLoad = {
+        "players":data.slice(1).map(player => ({
+          'name':player[0],
+          'expertise':player[1]
+        }))
+    }
+    console.log(payLoad);
+      await trigger(payLoad);
+      setShow(false);
+      listPlayers();
+      dispatch(
+        setToast({
+          severity: 'success',
+          summary: 'Saved',
+          detail: "Player saved successfully",
+          life: 3000,
+        }
+      ))  
+    } catch(err){
+      await trigger(data);
+      setShow(false);
+      listPlayers();
+      dispatch(
+        setToast({
+          severity: 'error',
+          summary: 'Please select  11 players.',
+          detail: err.response.data?.players || err.response.data,
+          life: 3000,
+        }
+      ))  
+    }
+  };
+
+  return { bulkUploadPlayers, loading, data };
+};
+export default useBulkUploadPlayers ;
