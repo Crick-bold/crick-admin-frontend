f16dbdaf426b0e4642e7d00a46598bfd47890d22
Ankit Kumar <ankit.kumar@cashfree.com>
2024-12-05 00:24:40 +0530

    Merge conflict resolved

--- src/pages/Series/index.js
+++ src/pages/Series/index.js
@@ -1,4 +1,4 @@
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import useGetSeries from "./hooks/useGetSeries";
 import styles from "./styles.module.css";
 import Tabs from "../Components/Tabs";
@@ -8,82 +8,63 @@ import Table from "../Components/Table";
 import {
   batsmanTableColumns,
   bowlerTableColumns,
+  top4sPlayers,
+  top6sPlayers,
 } from "./utlis/top-perfromers-columns";
+import SeriesMatches from "./SeriesMatches";
 
 const Series = () => {
   const { id: seriesId } = useParams();
-    const [active, setActive] = useState(0);
-  
-  const { loading, data: seriesData } = useGetSeries({ seriesId });
-   
-    
-    return (
+  const [active, setActive] = useState(0);
+  const { loading, data: seriesData, getSeries } = useGetSeries({ seriesId });
+
+  return (
     <>
       <div className={styles.series_heading}>
         <img src={seriesData?.imageUrl} className={styles.series_image} />
         <div>{seriesData?.name}</div>
+        <div className="text-16 color-highlight">
+          ({seriesData?.overs} overs)
+        </div>
       </div>
       <Tabs
-        tabs={["Matches", "Top Performers", "News"]}
+        tabs={["Matches", "Leaderboard", "News"]}
         active={active}
         onChange={(index) => setActive(index)}
       />
       {active === 0 && (
-        <div>
-          <div className={styles.matchStatus}>
-            <div className={styles.matchStatusHeader}>Live Match</div>
-            <div className={styles.matchContainer}>
-              {!loading &&
-                seriesData?.liveMatches?.length===0?
-                <div className={styles.noMatch}> No Live match</div>
-                :
-                seriesData?.liveMatches?.map((match, index) => (
-                  <MatchCard match={match} key={index} />
-                ))}
-            </div>
-          </div>
-          <div className={styles.matchStatus}>
-            <div className={styles.matchStatusHeader}>Upcoming Matches</div>
-                        <div className={styles.matchContainer}>
-                          
-              {!loading &&
-                              seriesData?.upcomingMatches?.length === 0 ?
-                              
-                                (<div className={styles.noMatch}>No upcoming Matches</div>
-                                   
-                              
-                         
-                )
-                :
-                seriesData?.upcomingMatches?.map((match, index) => (
-                  <MatchCard match={match} key={index} />
-                ))}
-            </div>
-          </div>
-          <div className={styles.matchStatus}>
-            <div className={styles.matchStatusHeader}>Past Matches</div>
-            <div className={styles.matchContainer}>
-              {!loading &&
-                seriesData?.pastMatches?.length===0?
-                <div className={styles.noMatch} >No past matches to display</div>
-                :
-                seriesData?.pastMatches?.map((match, index) => (
-                  <MatchCard match={match} key={index} />
-                ))}
-            </div>
-          </div>
-        </div>
+        <SeriesMatches
+          loading={loading}
+          seriesData={seriesData}
+          getSeries={getSeries}
+        />
       )}
       {active === 1 && (
         <div className={styles.topPerformerContainer}>
-          <Table
-            columns={batsmanTableColumns}
-            data={seriesData?.topPerformers?.batsmans}
-          />
-          <Table
-            columns={bowlerTableColumns}
-            data={seriesData?.topPerformers?.bowlers}
-          />
+          <div>
+            <div className={styles.title}>Runs</div>
+            <Table
+              columns={batsmanTableColumns}
+              data={seriesData?.topPerformers?.batsmen}
+            />
+            <div className={styles.title}>Wickets</div>
+            <Table
+              columns={bowlerTableColumns}
+              data={seriesData?.topPerformers?.bowlers}
+            />
+          </div>
+          <div>
+            <div className={styles.title}>Most 4s</div>
+            <Table
+              columns={top4sPlayers}
+              data={seriesData?.topPerformers?.top4sBatsmen}
+            />
+            <div className={styles.title}>Most 6s</div>
+            <Table
+              columns={top6sPlayers}
+              data={seriesData?.topPerformers?.top6sBatsmen}
+            />
+          </div>
         </div>
       )}
     </>
