ce10ddf3d0f1f7523b0de7b34515a753354e9a84
Ankit Kumar <ankit.kumar@cashfree.com>
2024-12-05 21:27:52 +0530

    build added

--- src/pages/Navigation/Login.js
+++ src/pages/Navigation/Login.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import { GoogleLogin } from '@react-oauth/google'
-const Login = ({responseMessage,errorMessage}) => {
-    
+import React from "react";
+import { GoogleLogin } from "@react-oauth/google";
+import Button from "../Components/Button";
+import styles from "./styles.module.css";
+const Login = ({ responseMessage, errorMessage, text = "Login" }) => {
   return (
-  
-          <GoogleLogin
-            onSuccess={responseMessage}
-            onError={errorMessage}
-            theme="outline"
-            size="medium"
-            text="signin"
-          
-          />
-  )
-}
+    <div className={styles.login_btn}>
+      <Button value={text} />
+      <div style={{ opacity: 0 }}>
+        <GoogleLogin
+          onSuccess={responseMessage}
+          onError={errorMessage}
+          theme="outline"
+          size="medium"
+          text="signin"
+          width="120px"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,19 +1,18 @@
 // import IconScreen from "../Components/Icons/IconScreen";
 
 import styles from "./styles.module.css";
-import img7 from "./image7.svg"
-import img2 from "./img2.png"
-import img3 from "./img3.png"
-import img4 from "./img4.png"
+import img7 from "./image7.svg";
+import img2 from "./img2.png";
+import img3 from "./img3.png";
+import img4 from "./img4.png";
 import Login from "../Navigation/Login";
-import useAuth from '../Navigation/hooks/useAuth'
+import useAuth from "../Navigation/hooks/useAuth";
 import { useEffect } from "react";
 const Welcome = ({ user, setUser, setUserLoaded }) => {
   const { responseMessage, errorMessage } = useAuth({ setUser, setUserLoaded });
   useEffect(() => {
     responseMessage({ credential: localStorage.getItem("token") });
-  }, [])
-
+  }, []);
 
   return (
     <>
@@ -21,71 +20,95 @@ const Welcome = ({ user, setUser, setUserLoaded }) => {
         <div className={styles["navbar"]}>
           <div className={styles["navbar-text"]}>Crick Bold</div>
         </div>
-      
+
         <div className={styles["container-1"]}>
           <div className={styles["container-1-1"]}>
             <div className={styles["container-1-1-text"]}>
-              <div className={styles["container-1-1-text1"]}>Say No to Pen & Paper</div>
-              
+              <div className={styles["container-1-1-text1"]}>
+                Say No to Pen & Paper
+              </div>
+
               <p>Organise Cricket Tournament in Just few Click</p>
               {/* <button onClick={()=>setUser(!user)}>START NOW</button> */}
-              <Login responseMessage={responseMessage} errorMessage={errorMessage} />
+              <div className="center-div">
+                <Login
+                  responseMessage={responseMessage}
+                  errorMessage={errorMessage}
+                  text="START NOW"
+                />
+              </div>
             </div>
             <div className={styles["container-1-1-img"]}>
-              <img src={img7} alt='img1' />
+              <img src={img7} alt="img1" />
             </div>
           </div>
           <div className={styles["what-crick"]}>What we do ?</div>
-          <hr className={ styles["horizontal-line"]} />
+          <hr className={styles["horizontal-line"]} />
         </div>
-       
+
         <div className={styles["container-2"]}>
-          
           <div className={styles["container-2-text"]}>
             <div className={styles["container-2-text-2"]}>HOST TOURNAMENTS</div>
-            <p>Create and mangage cricket tournaments with <br />ease.Set up matches, manage teams,and keep track <br />of scores all in one place.</p>
+            <p>
+              Create and mangage cricket tournaments with <br />
+              ease.Set up matches, manage teams,and keep track <br />
+              of scores all in one place.
+            </p>
             {/* <button>HOST NOW</button> */}
-            <Login responseMessage={responseMessage} errorMessage={errorMessage} />
-           
+            <Login
+              responseMessage={responseMessage}
+              errorMessage={errorMessage}
+              text="HOST NOW"
+            />
           </div>
           <div className={styles["container-2-img"]}>
-            <img src={img2} alt='img2' />
+            <img src={img2} alt="img2" />
           </div>
-    
         </div>
-         <hr className={styles["horizontal-line"]} />
+        <hr className={styles["horizontal-line"]} />
         <div className={styles["container-3"]}>
-              
           <div className={styles["container-2-img"]}>
-            <img src={img3} alt='img3' />
+            <img src={img3} alt="img3" />
           </div>
           <div className={styles["container-2-text"]}>
             <div className={styles["container-2-text-2"]}>MANAGE TEAMS</div>
-            <p>Easily build teams by adding players, customizing<br />names, and assigning roles Schedule matches,
-              and track <br /> progess-all in one place.</p>
+            <p>
+              Easily build teams by adding players, customizing
+              <br />
+              names, and assigning roles Schedule matches, and track <br />{" "}
+              progess-all in one place.
+            </p>
             {/* <button>CREATE NOW</button> */}
-            <Login responseMessage={responseMessage} errorMessage={errorMessage} />
+            <Login
+              responseMessage={responseMessage}
+              errorMessage={errorMessage}
+              text="CREATE NOW"
+            />
           </div>
-              
-    
-          
         </div>
         <hr className={styles["horizontal-line"]} />
         <div className={styles["container-4"]}>
-             
           <div className={styles["container-2-text"]}>
             <div className={styles["container-2-text-2"]}>SCHEDULE MATCH</div>
-            <p>Set match dates, times, and venues in just a few clicks<br />Create a seamless schedule that keeps teams <br /> and
-              fans on track</p>
+            <p>
+              Set match dates, times, and venues in just a few clicks
+              <br />
+              Create a seamless schedule that keeps teams <br /> and fans on
+              track
+            </p>
             {/* <button>SCHEDULE NOW</button> */}
-            <Login responseMessage={responseMessage} errorMessage={errorMessage} />
+            <Login
+              responseMessage={responseMessage}
+              errorMessage={errorMessage}
+              text="SCHEDULE NOW"
+            />
           </div>
           <div className={styles["container-2-img"]}>
-            <img src={img4} alt='img4' />
+            <img src={img4} alt="img4" />
           </div>
         </div>
       </div>
     </>
-  )
+  );
 };
-export default  Welcome;
+export default Welcome;
